e6da00e0c00edd2f50adb73ba981efa17d57a56c
Marcel Kloubert <marcel.kloubert@e-go-digital.com>
2019-05-20 15:24:23 +0200

    docker-up command

---
+++ src/commands/docker-up/index.ts
@@ -0,0 +1,43 @@
+/**
+ * This file is part of the ego-cli distribution.
+ * Copyright (c) e.GO Digital GmbH, Aachen, Germany (https://www.e-go-digital.com/)
+ *
+ * ego-cli is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * ego-cli is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as _ from 'lodash';
+import { CommandBase, CommandExecuteContext } from '../../contracts';
+import { spawn, writeLine } from '../../util';
+
+
+/**
+ * Docker-Stop command.
+ */
+export class EgoCommand extends CommandBase {
+    /** @inheritdoc */
+    public readonly description = "Shorthand for 'docker-compse up'.";
+
+    /** @inheritdoc */
+    public async execute(ctx: CommandExecuteContext): Promise<void> {
+        spawn('docker-compose', ['up']);
+    }
+
+    /** @inheritdoc */
+    public async showHelp(): Promise<void> {
+        writeLine(`Options:`);
+        writeLine(` -v, --verbose  # Verbose output.`);
+        writeLine();
+
+        writeLine(`Example:    ego docker-up`);
+    }
+}
